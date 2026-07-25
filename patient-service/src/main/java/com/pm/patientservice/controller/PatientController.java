68a9010416a3da564b36e101b982eb3b098a2662
Swetabh48 <swetabhsalampuria48@gmail.com>
2026-07-26 02:13:52 +0530

    Add update and delete endpoints with unique email enforcement.

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -6,10 +6,14 @@ import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
 import com.pm.patientservice.service.PatientService;
 import jakarta.validation.groups.Default;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +43,21 @@ public class PatientController {
         patientRequestDTO);
 
     return ResponseEntity.ok().body(patientResponseDTO);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id,
+      @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
+
+    PatientResponseDTO patientResponseDTO = patientService.updatePatient(id,
+        patientRequestDTO);
+
+    return ResponseEntity.ok().body(patientResponseDTO);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+    patientService.deletePatient(id);
+    return ResponseEntity.noContent().build();
   }
 }
